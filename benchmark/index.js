c81dd0670a62289b711fd5ab275873df673f1705
Alex Nault <nault.alex@gmail.com>
2022-08-01 20:34:21 -0400

    Fix benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -2,7 +2,7 @@ const benchmark = require("benchmark");
 const classnames = require("classnames");
 const { clsx } = require("clsx");
 
-const { cx } = require("../dist/classix.cjs");
+const { cx } = require("../dist/cjs/classix.js");
 
 const args = ["class1", "class2", false && "class3", true && "class4"];
 
