4aa65be41860f0855140fa4bae9e1d308a3a7c88
Alex Nault <nault.alex@gmail.com>
2022-08-04 22:01:33 -0400

    chore: Fix args spread bias in benchmark and compare with published classix (#24)
    * chore: Fix args spread bias in benchmark and compare with published classix
    
    * Update perf image

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,14 +1,14 @@
 const benchmark = require("benchmark");
 const classnames = require("classnames");
 const { clsx } = require("clsx");
+const { cx } = require("classix");
 
-const { cx } = require("../dist/cjs/classix.js");
-
-const args = ["class1", "class2", false && "class3", true && "class4"];
+const { cx: cxLocal } = require("../dist/cjs/classix.js");
 
 new benchmark.Suite()
-  .add("classnames", () => classnames(...args))
-  .add("clsx", () => clsx(...args))
-  .add("classix", () => cx(...args))
+  .add("classnames", () => classnames("class1", true && "class2"))
+  .add("clsx", () => clsx("class1", true && "class2"))
+  .add("classix", () => cx("class1", true && "class2"))
+  .add("classix (local)", () => cxLocal("class1", true && "class2"))
   .on("cycle", (event) => console.log(event.target.toString()))
   .run();
