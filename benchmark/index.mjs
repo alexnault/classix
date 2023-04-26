2fc99a1f117dfbcde845f9ae156188d776bbdb60
Alex Nault <nault.alex@gmail.com>
2023-04-25 22:57:06 -0400

    docs: Compare performance with object api (#49)
    * docs: Add object api perf comparison
    
    * Simplify benchmark file

--- benchmark/index.mjs
+++ benchmark/index.mjs
@@ -1,14 +1,69 @@
-import benchmark from "benchmark";
+import { performance } from "node:perf_hooks";
 import classnames from "classnames";
 import { clsx } from "clsx";
 import { cx } from "classix";
 
 import { cx as cxLocal } from "../dist/esm/classix.mjs";
 
-new benchmark.Suite()
-  .add("classnames", () => classnames("class1", true && "class2"))
-  .add("clsx", () => clsx("class1", true && "class2"))
-  .add("classix", () => cx("class1", true && "class2"))
-  .add("classix (local)", () => cxLocal("class1", true && "class2"))
-  .on("cycle", (event) => console.log(event.target.toString()))
-  .run();
+const NB_RUN = 100_000_000;
+
+function logResult(name, ms) {
+  const millionsOfOpsPerSecond = ((NB_RUN * 1000) / ms / 1_000_000).toFixed(1);
+  console.log(`${name}: ${millionsOfOpsPerSecond}M ops/s`);
+}
+
+// Repetition of code was used as to not affect performance
+let start = 0;
+let stop = 0;
+
+start = performance.now();
+for (let i = 0; i < NB_RUN; i++) {
+  classnames("class-1", { "class-2": true });
+}
+stop = performance.now();
+logResult("classnames (object)", stop - start);
+
+//
+
+start = performance.now();
+for (let i = 0; i < NB_RUN; i++) {
+  classnames("class-1", true && "class-2");
+}
+stop = performance.now();
+logResult("classnames", stop - start);
+
+//
+
+start = performance.now();
+for (let i = 0; i < NB_RUN; i++) {
+  clsx("class-1", { "class-2": true });
+}
+stop = performance.now();
+logResult("clsx (object)", stop - start);
+
+//
+
+start = performance.now();
+for (let i = 0; i < NB_RUN; i++) {
+  clsx("class-1", true && "class-2");
+}
+stop = performance.now();
+logResult("clsx", stop - start);
+
+//
+
+start = performance.now();
+for (let i = 0; i < NB_RUN; i++) {
+  cx("class-1", true && "class-2");
+}
+stop = performance.now();
+logResult("classix", stop - start);
+
+//
+
+start = performance.now();
+for (let i = 0; i < NB_RUN; i++) {
+  cxLocal("class-1", true && "class-2");
+}
+stop = performance.now();
+logResult("classix (local)", stop - start);
